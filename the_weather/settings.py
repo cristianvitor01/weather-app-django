b34eb055df95257228bb9a360b9e6447df216505
cristianvitor01 <cristianvitoralves@gmail.com>
2023-08-28 11:13:19 -0300

    vercel config

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qmh9$9(-@!*c$%w1w1o-eyj3_b3^b9r1%oo4=%to$!n3z4898+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'the_weather.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:dxNggwftQ63cMH3eiGIh@containers-us-west-47.railway.app:7047/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'dxNggwftQ63cMH3eiGIh',
+        'HOST': 'containers-us-west-47.railway.app',
+        'PORT': 7047,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
